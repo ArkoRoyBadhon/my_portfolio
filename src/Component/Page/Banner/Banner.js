8e0ea3acd7338e932f1d48b0c6cf22047b27ea3f
Arko <arkoroyb@gmail.com>
2022-12-10 22:40:41 +0600

    react-auto-typing implemented

--- src/Component/Page/Banner/Banner.js
+++ src/Component/Page/Banner/Banner.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import avatar from '../../../assets/87617-gentleman-thinking-about-money.gif'
 
+import AutoTyping, { BlinkCursor } from 'react-auto-typing'
+
 
 const Banner = () => {
 
@@ -11,8 +13,35 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={avatar} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Hi, I'm Arko Roy Badhon</h1>
-                    <h4 className="text-3xl font-bold">MERN STACK Developer</h4>
+                    {/* <h1 className="text-5xl font-bold">Hi, I'm Arko Roy Badhon</h1>
+                    <h4 className="text-3xl font-bold">MERN STACK Developer</h4> */}
+                    <div className="">
+                        <AutoTyping
+                            className="text-5xl font-bold"
+                            active // <boolean>
+                            textRef="Hi, I'm Arko Roy Badhon" // <string>
+                            writeSpeed={150} // <number>
+                            deleteSpeed={150} // <number>
+                            delayToWrite={1000} // <number>
+                            delayToDelete={2000} // <number>
+                        />
+                        <br />
+                        <AutoTyping
+                            className="text-3xl font-bold"
+                            active // <boolean>
+                            textRef="Full Stack Developer" // <string>
+                            writeSpeed={150} // <number>
+                            deleteSpeed={150} // <number>
+                            delayToWrite={1000} // <number>
+                            delayToDelete={2000} // <number>
+                        />
+
+                        <BlinkCursor
+                            active // <boolean>
+                            blinkSpeed={500} // <number>
+                        />
+                    </div>
+                    <br />
                     {/* <button onClick={handleDownload} className="btn btn-primary mt-10">My Resume</button> */}
                     <a className='btn btn-info mt-5' href="https://drive.google.com/file/d/12NAsHpf-gTMV5b7Mc0gha5KBwFJopygJ/view?usp=share_link" download>
                         <button>Dowload Resume</button>
