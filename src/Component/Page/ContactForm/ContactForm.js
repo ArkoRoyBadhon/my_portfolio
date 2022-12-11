597692b5ee70ff6a1e4cc48e02a87e6ccfae3529
Arko <arkoroyb@gmail.com>
2022-12-11 14:19:11 +0600

    messenger integreted

--- src/Component/Page/ContactForm/ContactForm.js
+++ src/Component/Page/ContactForm/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import toast from 'react-hot-toast';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
 const ContactForm = () => {
@@ -19,44 +20,51 @@ const ContactForm = () => {
     }
 
     return (
-        <div className='my-16' id="contact-form">
-            <div className="min-w-screen-lg">
+        <>
+            <div className='my-16' id="contact-form">
+                <div className="min-w-screen-lg">
 
-                <div className="card md:w-[50rem] bg-base-100 shadow-xl mx-auto">
-                    <div className="card-body">
-                        <h4 className="text-3xl font-bold text-center">Contact Form</h4>
-                        <form ref={form} onSubmit={sendEmail} className='px-10 pb-16 pt-10'>
-                            <div className="form-control w-full my-5">
-                                <label className="label">
-                                    <span className="label-text">Full Name</span>
-                                </label>
-                                <input required type="text" name="user_name" placeholder="full name" className="input input-bordered " />
-                            </div>
-                            <div className="form-control w-full my-5">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input required type="text" name='user_email' placeholder="email" className="input input-bordered w-full" />
-                            </div>
-                            <div className="form-control w-full my-5">
-                                <label className="label">
-                                    <span className="label-text">Address</span>
-                                </label>
-                                <input required type="text" name='user_address' placeholder="address" className="input input-bordered w-full" />
-                            </div>
-                            <div className="form-control w-full my-5">
-                                <label className="label">
-                                    <span className="label-text">Write Something</span>
-                                </label>
-                                <textarea required name='message' className="textarea textarea-bordered" placeholder="write you message"></textarea>
-                            </div>
-                            <input className='btn mx-auto' type="submit" value="Send Email" />
-                        </form>
+                    <div className="card md:w-[50rem] bg-base-100 shadow-xl mx-auto">
+                        <div className="card-body">
+                            <h4 className="text-3xl font-bold text-center">Contact Form</h4>
+                            <form ref={form} onSubmit={sendEmail} className='px-10 pb-16 pt-10'>
+                                <div className="form-control w-full my-5">
+                                    <label className="label">
+                                        <span className="label-text">Full Name</span>
+                                    </label>
+                                    <input required type="text" name="user_name" placeholder="full name" className="input input-bordered " />
+                                </div>
+                                <div className="form-control w-full my-5">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input required type="text" name='user_email' placeholder="email" className="input input-bordered w-full" />
+                                </div>
+                                <div className="form-control w-full my-5">
+                                    <label className="label">
+                                        <span className="label-text">Address</span>
+                                    </label>
+                                    <input required type="text" name='user_address' placeholder="address" className="input input-bordered w-full" />
+                                </div>
+                                <div className="form-control w-full my-5">
+                                    <label className="label">
+                                        <span className="label-text">Write Something</span>
+                                    </label>
+                                    <textarea required name='message' className="textarea textarea-bordered" placeholder="write you message"></textarea>
+                                </div>
+                                <input className='btn mx-auto' type="submit" value="Send Email" />
+                            </form>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
+            <MessengerCustomerChat
+                pageId="100088257536489"
+                appId="870290150669273"
+                htmlRef="<REF_STRING>"
+            />
+        </>
     );
 };
 export default ContactForm;
